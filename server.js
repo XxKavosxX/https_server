e857296e963bbdb924bcad09fecffdfb85221d3f
XxKavosxX <maikerdralcantara@gmail.com>
2021-02-24 12:18:54 -0300

    Update 2 server.js

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const options = {
 };
 
 https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end("OTA server is up!");
   fs.readFile(__dirname + req.url, function (err, data) {
     if (err) {
       res.writeHead(404);
-      res.end(JSON.stringify(err));
+      res.end("Nothing to show here:"+JSON.stringify(err));
       return;
+    }else{
+      res.writeHead(200);
+      res.end("File exist.");
     }
   });
 }).listen(8070);
